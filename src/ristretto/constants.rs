65e308c6fbfc48a9e6a4ee1908da13dd8bbe47ad
Hammad Tariq <hammadtariq@yahoo.co.uk>
2021-07-27 13:58:28 +0500

    simplified generate_account

--- src/ristretto/constants.rs
+++ src/ristretto/constants.rs
@@ -2,7 +2,7 @@ use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 
 // This is a temporary solution for declaring assets. It should be moved to a governance module
 // byte codes of static points on the curve are hard-coded as here - these were imported from rust - pedersengens function of bulletproofs
-pub const BASE_PAIR_BTC_COMPRESSED : [CompressedRistretto; 2] = [
+pub const BASE_PK_BTC_COMPRESSED : [CompressedRistretto; 2] = [
     CompressedRistretto([
         226, 242, 174, 10, 106, 188, 78, 113, 168, 132, 169, 97, 197, 0, 81, 95, 88, 227, 11, 106, 165, 130, 221, 141, 182, 166, 89, 69, 224, 141, 45, 118
     ]),

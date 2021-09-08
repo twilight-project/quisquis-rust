ef0e32b7848ed9e44bcc5fde04c2627b0245405b
usmanshahid86 <usman.shahid@nu.edu.pk>
2021-09-08 15:32:06 +0500

    Cleaned shuffle.rs of warnings
    Added tests for signature.rs

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,8 +1,10 @@
 pub mod transaction;
 pub mod shuffle;
-
+pub mod signature;
 // Re-export
 pub use self::{
     transaction::Transaction,
-    shuffle::Permutation
+    shuffle::Permutation,
+    signature::{Signature, VerificationKey, SigningKey}
+
 };

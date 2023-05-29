207e9b76197d630b91609fe4bd694990a7aacecc
Usman Shahid <26981965+usmanshahid86@users.noreply.github.com>
2023-05-29 21:46:17 +0500

    testing lib

--- src/accounts/accounts.rs
+++ src/accounts/accounts.rs
@@ -27,6 +27,7 @@ impl Account {
     /// generate_account creates a new account
     /// returns PublicKey, SecretKey and a Commitment with 0 balance and commitment scalar for Annoynimity account proof
     pub fn generate_account(pk: RistrettoPublicKey) -> (Account, Scalar) {
+        println!("Just checking the library update");
         // lets get a random scalar
         let comm_scalar = Scalar::random(&mut OsRng);
 

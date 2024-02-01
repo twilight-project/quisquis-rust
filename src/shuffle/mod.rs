7b74c83f359acf691cd904a5ebeee40f5b6b5db8
Hammad Tariq <hammadtariq@yahoo.co.uk>
2024-02-02 01:18:31 +0500

    added serialize deserialize for SecretKey

--- src/shuffle/mod.rs
+++ src/shuffle/mod.rs
@@ -4,10 +4,12 @@ mod product;
 pub mod shuffle;
 mod singlevalueproduct;
 mod vectorutil;
-// Re-export
-pub use self::{
-    shuffle::Permutation, shuffle::Shuffle, shuffle::ShuffleProof, shuffle::ShuffleStatement,
-    vectorutil::ScalarExp,
-};
 pub mod hadamard;
 pub mod polynomial;
+
+// Re-export structures, functions, and constants
+pub use self::{
+    shuffle::{Permutation, Shuffle, ShuffleProof, ShuffleStatement, N, ROWS, COLUMNS},
+    vectorutil::ScalarExp,
+    // ... other re-exports as needed
+};

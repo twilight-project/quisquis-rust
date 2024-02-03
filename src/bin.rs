883764e09441faea1efb1cf3c52704a855ef85e4
Usman Shahid <26981965+usmanshahid86@users.noreply.github.com>
2024-02-03 22:34:32 +0500

    multiexpo test

--- src/bin.rs
+++ src/bin.rs
@@ -1,107 +1,120 @@
-extern crate quisquislib; // not needed since Rust edition 2018
+// extern crate quisquislib; // not needed since Rust edition 2018
 
-use curve25519_dalek::scalar::Scalar;
-use quisquislib::{
-    accounts::Account,
-    //util::address::{Address}
-    elgamal::ElGamalCommitment,
-    keys::{PublicKey, SecretKey},
-    ristretto::{RistrettoPublicKey, RistrettoSecretKey},
-};
-use rand::rngs::OsRng;
-
+// use curve25519_dalek::scalar::Scalar;
+// use quisquislib::{
+//     accounts::Account,
+//     //util::address::{Address}
+//     elgamal::ElGamalCommitment,
+//     keys::{PublicKey, SecretKey},
+//     ristretto::{RistrettoPublicKey, RistrettoSecretKey},
+// };
+// use rand::rngs::OsRng;
+use std::panic;
+use wasm_bindgen::prelude::*;
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
 pub fn main() {
-    //test();
-    let sk: RistrettoSecretKey = SecretKey::random(&mut OsRng);
-    let pk = RistrettoPublicKey::from_secret_key(&sk, &mut OsRng);
-    println!("{:?}", sk);
-    println!("{:?}", pk);
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let _body = document.body().expect("document should have a body");
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    let (acc, _) = Account::generate_account(pk);
-    println!("generated account {:?}", acc);
-
-    let updated_keys_scalar = Scalar::random(&mut OsRng);
-
-    // lets get a random scalar
-    let comm_scalar = Scalar::random(&mut OsRng);
-
-    let updated_account =
-        Account::update_account(acc, 16u64.into(), updated_keys_scalar, comm_scalar);
-    println!("updated account {:?}", updated_account);
-
-    let _rsk: RistrettoSecretKey = SecretKey::random(&mut OsRng);
-
-    let random_scalar = Scalar::random(&mut OsRng);
-    let updated_pk = RistrettoPublicKey::update_public_key(&pk, random_scalar);
-    println!("{:?}", updated_pk);
-    let verify_public_key_update =
-        RistrettoPublicKey::verify_public_key_update(&updated_pk, &pk, random_scalar);
-    println!("{:?}", verify_public_key_update);
-
-    let generate_base_pk = RistrettoPublicKey::generate_base_pk();
-    println!("generate base pk {:?}", generate_base_pk);
-
-    let generate_commitmenta =
-        ElGamalCommitment::generate_commitment(&pk, random_scalar, 12u64.into());
-    println!("{:?}", generate_commitmenta);
-
-    let generate_commitmentb =
-        ElGamalCommitment::generate_commitment(&pk, random_scalar, 12u64.into());
-    println!("{:?}", generate_commitmentb);
-
-    let add_commitments =
-        ElGamalCommitment::add_commitments(&generate_commitmenta, &generate_commitmentb);
-    println!("added commitments here {:?}", add_commitments);
-    let _num = 10 as u64;
-    // let sign_int = SignedInteger::from(num);
-    // let neg_sign_int = -sign_int;
-    // println!("int = {:?}, Sign Int = {:?}", sign_int, neg_sign_int);
-    // let possscalar: Scalar = SignedInteger::into(sign_int);
-    // let negscalar: Scalar = SignedInteger::into(neg_sign_int);
-    // println!("Scalar = {:?}, Sign Int= {:?}", possscalar, negscalar);
-
-    // lets create a new keypair
-    let mut rng = rand::thread_rng();
-    let sk: RistrettoSecretKey = SecretKey::random(&mut rng);
-    let pk = RistrettoPublicKey::from_secret_key(&sk, &mut rng);
-
-    let (acc, _) = Account::generate_account(pk);
-    println!("generated account {:?}", acc);
-
-    let updated_keys_scalar = Scalar::random(&mut OsRng);
-
-    // lets get a random scalar
-    let comm_scalar = Scalar::random(&mut OsRng);
-
-    let updated_account =
-        Account::update_account(acc, 16u64.into(), updated_keys_scalar, comm_scalar);
-    println!("updated account {:?}", updated_account);
-
-    //let rscalar = Scalar::random(&mut OsRng);
-
-    //     let create_delta_account = Account::create_delta_account(updated_account, 5, rscalar);
-    //     println!("create_delta_account {:?}", create_delta_account);
-
-    //     let create_epsilon_account = Account::create_epsilon_account(16, rscalar, generate_base_pk);
-    //     println!("create_epsilon_account {:?}", create_epsilon_account);
-
-    //     let updated_delta_account = Account::update_delta_account(updated_account, create_delta_account);
-    //     println!("updated_delta_account {:?}", updated_delta_account.unwrap());
-
-    //     println!("{:?}", pk.as_bytes());
-    //     let net = quisquislib::util::address::Network::default();
-    //     let addr = Address::standard(net,pk);
-    //    // println!("{:?}", addr.as_bs58());
-    //     println!("{:?}", addr.as_hex());
-    //     println!("{:?}", addr);
-    //     let addr_hex = addr.as_hex();
-
-    //     let decoded_address = Address::from_hex(&addr_hex);
-    //     println!("{:?}", decoded_address);
-
-    //     let updated_delta_account = Account::update_delta_account(updated_account, create_delta_account).unwrap();
-    //     println!("updated_delta_account {:?}", updated_delta_account);
-
-    //     let updated_delta_account = Account::verify_delta_update(updated_delta_account, create_delta_account, updated_account);
-    //     println!("updated_delta_account {:?}", updated_delta_account);
+    //Ok(())
 }
+// pub fn main() {
+//     //test();
+//     let sk: RistrettoSecretKey = SecretKey::random(&mut OsRng);
+//     let pk = RistrettoPublicKey::from_secret_key(&sk, &mut OsRng);
+//     println!("{:?}", sk);
+//     println!("{:?}", pk);
+
+//     let (acc, _) = Account::generate_account(pk);
+//     println!("generated account {:?}", acc);
+
+//     let updated_keys_scalar = Scalar::random(&mut OsRng);
+
+//     // lets get a random scalar
+//     let comm_scalar = Scalar::random(&mut OsRng);
+
+//     let updated_account =
+//         Account::update_account(acc, 16u64.into(), updated_keys_scalar, comm_scalar);
+//     println!("updated account {:?}", updated_account);
+
+//     let _rsk: RistrettoSecretKey = SecretKey::random(&mut OsRng);
+
+//     let random_scalar = Scalar::random(&mut OsRng);
+//     let updated_pk = RistrettoPublicKey::update_public_key(&pk, random_scalar);
+//     println!("{:?}", updated_pk);
+//     let verify_public_key_update =
+//         RistrettoPublicKey::verify_public_key_update(&updated_pk, &pk, random_scalar);
+//     println!("{:?}", verify_public_key_update);
+
+//     let generate_base_pk = RistrettoPublicKey::generate_base_pk();
+//     println!("generate base pk {:?}", generate_base_pk);
+
+//     let generate_commitmenta =
+//         ElGamalCommitment::generate_commitment(&pk, random_scalar, 12u64.into());
+//     println!("{:?}", generate_commitmenta);
+
+//     let generate_commitmentb =
+//         ElGamalCommitment::generate_commitment(&pk, random_scalar, 12u64.into());
+//     println!("{:?}", generate_commitmentb);
+
+//     let add_commitments =
+//         ElGamalCommitment::add_commitments(&generate_commitmenta, &generate_commitmentb);
+//     println!("added commitments here {:?}", add_commitments);
+//     let _num = 10 as u64;
+//     // let sign_int = SignedInteger::from(num);
+//     // let neg_sign_int = -sign_int;
+//     // println!("int = {:?}, Sign Int = {:?}", sign_int, neg_sign_int);
+//     // let possscalar: Scalar = SignedInteger::into(sign_int);
+//     // let negscalar: Scalar = SignedInteger::into(neg_sign_int);
+//     // println!("Scalar = {:?}, Sign Int= {:?}", possscalar, negscalar);
+
+//     // lets create a new keypair
+//     let mut rng = rand::thread_rng();
+//     let sk: RistrettoSecretKey = SecretKey::random(&mut rng);
+//     let pk = RistrettoPublicKey::from_secret_key(&sk, &mut rng);
+
+//     let (acc, _) = Account::generate_account(pk);
+//     println!("generated account {:?}", acc);
+
+//     let updated_keys_scalar = Scalar::random(&mut OsRng);
+
+//     // lets get a random scalar
+//     let comm_scalar = Scalar::random(&mut OsRng);
+
+//     let updated_account =
+//         Account::update_account(acc, 16u64.into(), updated_keys_scalar, comm_scalar);
+//     println!("updated account {:?}", updated_account);
+
+//     //let rscalar = Scalar::random(&mut OsRng);
+
+//     //     let create_delta_account = Account::create_delta_account(updated_account, 5, rscalar);
+//     //     println!("create_delta_account {:?}", create_delta_account);
+
+//     //     let create_epsilon_account = Account::create_epsilon_account(16, rscalar, generate_base_pk);
+//     //     println!("create_epsilon_account {:?}", create_epsilon_account);
+
+//     //     let updated_delta_account = Account::update_delta_account(updated_account, create_delta_account);
+//     //     println!("updated_delta_account {:?}", updated_delta_account.unwrap());
+
+//     //     println!("{:?}", pk.as_bytes());
+//     //     let net = quisquislib::util::address::Network::default();
+//     //     let addr = Address::standard(net,pk);
+//     //    // println!("{:?}", addr.as_bs58());
+//     //     println!("{:?}", addr.as_hex());
+//     //     println!("{:?}", addr);
+//     //     let addr_hex = addr.as_hex();
+
+//     //     let decoded_address = Address::from_hex(&addr_hex);
+//     //     println!("{:?}", decoded_address);
+
+//     //     let updated_delta_account = Account::update_delta_account(updated_account, create_delta_account).unwrap();
+//     //     println!("updated_delta_account {:?}", updated_delta_account);
+
+//     //     let updated_delta_account = Account::verify_delta_update(updated_delta_account, create_delta_account, updated_account);
+//     //     println!("updated_delta_account {:?}", updated_delta_account);
+// }
